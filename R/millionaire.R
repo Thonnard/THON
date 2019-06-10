40389ad13f5c2ea1f9188a056173d201f3f8a50a
David Thonnard <thonnard.david@gmail.com>
2019-06-10 20:07:56 +0200

    add days

--- R/millionaire.R
+++ R/millionaire.R
@@ -2,16 +2,14 @@
 #'
 #' These functions will make you rich
 #' goal: how many days to reach your goal
-#' days: how much money after x days
+#'
 #' @param m start money
 #' @param p daily percentage profit - default: 2
 #' @param g goal, default 1 million
-#' @param d days of trading
 #' @keywords money
 #' @export
 #' @examples
 #' goal(5000,2,100000)
-#' days(100,2,5000)
 
 goal <- function(m, p = 2, g = 1000000) {
   n <- 0
@@ -21,6 +19,15 @@ goal <- function(m, p = 2, g = 1000000) {
   }
   return(n)
 }
+
+#' days: how much money after x days
+#' @param d days of trading
+#' @param m start money
+#' @param p daily percentage profit - default: 2
+#' @keywords money
+#' @export
+#' @examples
+#' days(100,2,5000)
 
 days <- function(d, p = 2, m = 7000) {
   n <- 0
